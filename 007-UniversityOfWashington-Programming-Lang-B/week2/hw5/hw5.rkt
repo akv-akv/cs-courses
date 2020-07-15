5d7b06709f34b54f0ffab1aa200b599297dd0640
akv-akv <drzoob@yandex.ru>
2020-07-15 16:11:44 +0300

    update 007 hw5

--- 007-UniversityOfWashington-Programming-Lang-B/week2/hw5/hw5.rkt
+++ 007-UniversityOfWashington-Programming-Lang-B/week2/hw5/hw5.rkt
@@ -17,12 +17,27 @@
 (struct aunit ()    #:transparent) ;; unit value -- good for ending a list
 (struct isaunit (e) #:transparent) ;; evaluate to 1 if e is unit else 0
 
+
 ;; a closure is not in "source" programs but /is/ a MUPL value; it is what functions evaluate to
 (struct closure (env fun) #:transparent) 
 
 ;; Problem 1
 
-;; CHANGE (put your solutions here)
+;; (a) Write a Racket function racketlist->mupllist that takes a Racket list (presumably of mupl
+;; values but that will not affect your solution) and produces an analogous mupl list with the
+;; same elements in the same order.
+(define (racketlist->mupllist lst)
+  (cond
+    [(null? lst) (aunit)]
+    [#t (apair (car lst) (racketlist->mupllist (cdr lst)))]))
+
+;; (b) Write a Racket function mupllist->racketlist that takes a mupl list (presumably of mupl
+;; values but that will not affect your solution) and produces an analogous Racket list (of mupl values)
+;; with the same elements in the same order.
+(define (mupllist->racketlist mlst)
+  (cond
+    [(aunit? mlst) null]
+    [#t (cons (eval-exp (fst mlst)) (mupllist->racketlist (eval-exp (snd mlst))))]))
 
 ;; Problem 2
 
@@ -49,6 +64,45 @@
                        (int-num v2)))
                (error "MUPL addition applied to non-number")))]
         ;; CHANGE add more cases here
+        [(int? e) e]
+        [(ifgreater? e) 
+         (let ([v1 (eval-under-env (ifgreater-e1 e) env)]
+               [v2 (eval-under-env (ifgreater-e2 e) env)]
+               [v3 (eval-under-env (ifgreater-e3 e) env)]
+               [v4 (eval-under-env (ifgreater-e4 e) env)])
+           (if (and (int? v1)
+                    (int? v2))
+               (if (> (int-num v1) (int-num v2))
+                   v3
+                   v4)
+               (error "MUPL ifgreater applied to non-number")))]
+        [(fun? e) (closure env e)]
+        [(closure? e) e]
+        [(call? e) (letrec ([cl (eval-under-env (call-funexp e) env)]
+                         [ar (eval-under-env (call-actual e) env)]
+                         [fu (cond [(closure? cl) (closure-fun cl)]
+                                   [#t (error "MUPL call first argument should be closure")])]
+                         [en (cond
+                               [(equal? (fun-nameopt fu) #f) (cons (cons (fun-formal fu) ar) (closure-env cl))]
+                               [#t (cons (cons (fun-nameopt fu) cl) (cons (cons (fun-formal fu) ar) (closure-env cl)))])])
+                     (eval-under-env (fun-body fu) en))]
+        [(mlet? e) (letrec ([env0 (eval-under-env (mlet-e e) env)]
+                            [env1 (cons (cons (mlet-var e) env0) env)])
+                     (eval-under-env (mlet-body e) env1))]
+        [(apair? e) e]
+        [(fst? e) (let ([v (eval-under-env (fst-e e) env)])
+                    (if (apair? v)
+                        (apair-e1 v)
+                        (error "MUPL fst applied to non-apair")))]
+        [(snd? e) (let ([v (eval-under-env (snd-e e) env)])
+                    (if (apair? v)
+                        (apair-e2 v)
+                        (error "MUPL snd applied to non-apair")))]
+        [(aunit? e) e]
+        [(isaunit? e) (let ([v (eval-under-env (isaunit-e e) env)])
+                        (cond
+                        [(aunit? v) (int 1)]
+                        [#t (int 0)]))]
         [#t (error (format "bad MUPL expression: ~v" e))]))
 
 ;; Do NOT change
@@ -57,11 +111,22 @@
         
 ;; Problem 3
 
-(define (ifaunit e1 e2 e3) "CHANGE")
+(define (ifaunit e1 e2 e3)
+  (ifgreater (isaunit e1) (int 0) e2 e3))
 
-(define (mlet* lstlst e2) "CHANGE")
+(define (mlet* lstlst e2)
+  (cond
+    [(null? lstlst) e2]
+    [#t (let ([v (car lstlst)])
+          (mlet (car v) (cdr v) (mlet* (cdr lstlst) e2)))]))
 
-(define (ifeq e1 e2 e3 e4) "CHANGE")
+(define (ifeq e1 e2 e3 e4)
+  (mlet* (list (cons "x1" e1) (cons "x2" e2))
+         (ifgreater (var "x1") (var "x2")
+                    e4
+                    (ifgreater (var "x2") (var "x1")
+                               e4
+                               e3))))
 
 ;; Problem 4
 
