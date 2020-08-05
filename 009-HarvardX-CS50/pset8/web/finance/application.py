8416d97c5e348dfc24efb3f45186a0abc3aac501
Kirill Avilenko <kirill.avilenko@gmail.com>
2020-08-05 14:04:25 +0000

    update 009 pset8 finance

--- 009-HarvardX-CS50/pset8/web/finance/application.py
+++ 009-HarvardX-CS50/pset8/web/finance/application.py
@@ -45,21 +45,97 @@ if not os.environ.get("API_KEY"):
 @login_required
 def index():
     """Show portfolio of stocks"""
-    return apology("TODO")
+    total = 0
+
+    # Query database for all transactions of the user
+    data = db.execute("""
+                      SELECT transactions.symbol, name, SUM(shares)
+                      FROM transactions
+                      JOIN symbols ON symbols.symbol = transactions.symbol
+                      WHERE user_id=:user_id
+                      GROUP BY transactions.symbol
+                      HAVING SUM(shares) > 0""", user_id=session['user_id'])
+
+    # Add price and total to the dataset
+    for row in data:
+        row['price'] = lookup(row['symbol'])['price']
+        row['total'] = row['price'] * row['SUM(shares)']
+        total += row['total']
+
+    # Counting cash and total
+    cash = float(db.execute("""SELECT cash FROM users WHERE id=:user_id""",
+                            user_id=session['user_id'])[0]['cash'])
+    total += cash
+
+    # Render template with user's portfolio
+    return render_template('index.html', rows=data, cash=round(cash, 2), total=round(total, 2))
 
 
 @app.route("/buy", methods=["GET", "POST"])
 @login_required
 def buy():
     """Buy shares of stock"""
-    return apology("TODO")
+
+    # If user reaches route via POST
+    if request.method == "POST":
+        # Check if symbol is submitted
+        if not request.form.get("symbol"):
+            return apology("must provide symbol")
+
+        # Check if number of shares is filled and submitted
+        if not request.form.get("shares"):
+            return apology("must provide number of shares")
+
+        # Lookup for a symbol
+        data = lookup(request.form.get("symbol"))
+
+        # If lookup was unsuccessfull, show warning page
+        if not data:
+            return apology("invalid symbol")
+
+        # Else make necessary transacions
+        else:
+            # Check if user has enough cash
+            cash = db.execute("SELECT cash FROM users WHERE id=:user_id", user_id=session['user_id'])
+            purchase_amount = round(float(data['price']) * int(request.form.get("shares")), 2)
+            if cash[0]['cash'] < purchase_amount:
+                return apology("not enough money")
+            else:
+                # Reduce cash value by purchase ammount
+                db.execute("UPDATE users SET cash=:cash WHERE id=:user_id",
+                           cash=cash[0]['cash'] - purchase_amount, user_id=session['user_id'])
+                # Insert new record to transactions table
+                db.execute("""INSERT INTO transactions (user_id, symbol, shares, price, timestamp)
+                           VALUES (:user_id, :symbol, :shares, :price, CURRENT_TIMESTAMP)""",
+                           user_id=session['user_id'], symbol=data['symbol'],
+                           shares=int(request.form.get("shares")), price=round(float(data['price']), 2))
+                # Check if company name is in database:
+                name = db.execute("""SELECT * FROM symbols WHERE symbol=:symbol""", symbol=data['symbol'])
+                if not name:
+                    db.execute("""INSERT INTO symbols (symbol, name)
+                               VALUES (:symbol, :name)""",
+                               symbol=data['symbol'], name=data['name'])
+            return redirect("/")
+
+    # If user reaches route via GET
+    else:
+        return render_template("buy.html")
 
 
 @app.route("/history")
 @login_required
 def history():
     """Show history of transactions"""
-    return apology("TODO")
+    # Query database for all transactions of the user
+    data = db.execute("""
+                      SELECT transactions.symbol, name, shares, price, timestamp
+                      FROM transactions
+                      JOIN symbols ON symbols.symbol = transactions.symbol
+                      WHERE user_id=:user_id
+                      """, user_id=session['user_id'])
+
+    # Render template with query data
+    return render_template("history.html", data=data)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -185,7 +261,53 @@ def register():
 @login_required
 def sell():
     """Sell shares of stock"""
-    return apology("TODO")
+
+    # If user reached route via POST
+    if request.method == "POST":
+
+        # Check if user has enough shares
+        symbol = request.form['symbol']
+        shares = int(request.form['shares'])
+        data = db.execute("""SELECT transactions.symbol, SUM(shares)
+                          FROM transactions
+                          JOIN symbols ON symbols.symbol = transactions.symbol
+                          WHERE user_id=:user_id AND transactions.symbol=:symbol
+                          GROUP BY transactions.symbol""", user_id=session['user_id'],
+                          symbol=symbol)
+        if data[0]['SUM(shares)'] < shares:
+            return apology("not enough shares")
+        elif shares < 1:
+            return apology("number of shares not valid")
+        else:
+            # Lookup for price
+            price = float(lookup(symbol)['price'])
+
+            # Add price * shares to user cash
+            purchase_amount = round(price * shares, 2)
+            cash = db.execute("SELECT cash FROM users WHERE id=:user_id", user_id=session['user_id'])
+            db.execute("UPDATE users SET cash=:cash WHERE id=:user_id",
+                       cash=cash[0]['cash'] + purchase_amount, user_id=session['user_id'])
+
+            # Insert new record to transactions table
+            db.execute("""INSERT INTO transactions (user_id, symbol, shares, price, timestamp)
+                       VALUES (:user_id, :symbol, :shares, :price, CURRENT_TIMESTAMP)""",
+                       user_id=session['user_id'], symbol=symbol,
+                       shares=-shares, price=price)
+            return redirect("/")
+
+    # If user reached route via GET
+    else:
+        # Query database for all transactions of the user
+        data = db.execute("""SELECT transactions.symbol, SUM(shares)
+                          FROM transactions
+                          JOIN symbols ON symbols.symbol = transactions.symbol
+                          WHERE user_id=:user_id
+                          GROUP BY transactions.symbol""", user_id=session['user_id'])
+
+        if not data:
+            return apology("nothing to sell")
+        else:
+            return render_template("sell.html", symbols=data, shares=data)
 
 
 def errorhandler(e):
