aa4a567174dfc882772e402981c397eb1b7252e9
akv-akv <drzoob@yandex.ru>
2020-09-04 15:07:29 +0300

    update 010 project 07

--- 010-Build-a-Modern-Computer/projects/07/vmtranslator.py
+++ 010-Build-a-Modern-Computer/projects/07/vmtranslator.py
@@ -8,6 +8,7 @@ class CodeWriter:
         self.parser = Parser(path)
         self.outputFileName = path[:len(path)-3] + '.asm'
         self.f= open(self.outputFileName, 'w')
+        self.counter = 0
     
     def translatePushPop(self):
         """ Translates push and pop commands and writes
@@ -61,7 +62,7 @@ class CodeWriter:
             if segment in ['temp', 'pointer', 'local', 'argument', 'this', 'that']:
                 # Put value from command value argument to D
                 self.value_to_D()
-                # Get data from registers
+                # Get address of register cell 
                 if segment == 'temp':
                     self.f.write('@5\n')
                     self.f.write('D=D+A\n')
@@ -82,12 +83,15 @@ class CodeWriter:
                     self.f.write('D=D+M\n')
                 else:
                     pass
-                # TODO
+                # Store address to @13
                 self.f.write('@13\n')
                 self.f.write('M=D\n')
+                # Copy value from stack to D
                 self.copy_from_stack_to_D()
+                #Get address from @13
                 self.f.write('@13\n')
                 self.f.write('A=M\n')
+                # Store value from D into this address
                 self.f.write('M=D\n')
                 
 
@@ -131,36 +135,116 @@ class CodeWriter:
 
             
     def translateArithmetic(self):
-        # TODO
         """ Translates arithetic commands and writes it 
         to file"""
         # Get command from parser
         command = self.parser.commandParts[0]
         self.f.write('// {}\n'.format(self.parser.command))
         if command == 'add':
-            self.copy_from_stack_to_D()
-            self.f.write('A=A-1\n')
-            self.f.write('D=D+M\n')
-            self.f.write('M=D\n')
+            #self.copy_from_stack_to_D()
+            #self.f.write('A=A-1\n')
+            #self.f.write('D=D+M\n')
+            #self.f.write('M=D\n')
+            self.pop_top_stack()
+            self.f.write('D=M\n')
+            self.pop_top_stack()
+            self.f.write('M=M+D\n')
+            self.increment_SP()
         elif command == 'sub':
-            self.copy_from_stack_to_D()
-            self.f.write('A=A-1\n')
-            self.f.write('D=M-D\n')
-            self.f.write('M=D\n')
+            #self.copy_from_stack_to_D()
+            #self.f.write('A=A-1\n')
+            #self.f.write('D=M-D\n')
+            #self.f.write('M=D\n')
+            self.pop_top_stack()
+            self.f.write('D=M\n')
+            self.pop_top_stack()
+            self.f.write('M=M-D\n')
+            self.increment_SP()
+
         elif command == 'neg':
-            self.f.write('')
+            self.pop_top_stack()
+            self.f.write('M=-M\n')
+            self.increment_SP()
         elif command == 'eq':
-            self.f.write('')
+            self.counter += 1
+            self.pop_top_stack()
+            self.f.write('D=M\n')
+            self.pop_top_stack()
+            self.f.write('D=M-D\n')
+            self.f.write('@NOT_EQ{}\n'.format(self.counter))
+            self.f.write('D;JNE\n')
+            self.f.write('@SP\n')
+            self.f.write('A=M\n')
+            self.f.write('M=-1\n')
+            self.f.write('@INC_STACK_POINTER_EQ{}\n'.format(self.counter))
+            self.f.write('0;JMP\n')
+            self.f.write('(NOT_EQ{})\n'.format(self.counter))
+            self.f.write('@SP\n')
+            self.f.write('A=M\n')
+            self.f.write('M=0\n')
+            self.f.write('(INC_STACK_POINTER_EQ{})\n'.format(self.counter))
+            self.increment_SP()
         elif command == 'gt':
-            self.f.write('')
+            self.counter += 1
+            self.pop_top_stack()
+            self.f.write('D=M\n')
+            self.pop_top_stack()
+            self.f.write('D=M-D\n')
+            self.f.write('@NOT_GT{}\n'.format(self.counter))
+            self.f.write('D;JLE\n')
+            self.f.write('@SP\n')
+            self.f.write('A=M\n')
+            self.f.write('M=-1\n')
+            self.f.write('@INC_STACK_POINTER_GT{}\n'.format(self.counter))
+            self.f.write('0;JMP\n')
+            self.f.write('(NOT_GT{})\n'.format(self.counter))
+            self.f.write('@SP\n')
+            self.f.write('A=M\n')
+            self.f.write('M=0\n')
+            self.f.write('(INC_STACK_POINTER_GT{})\n'.format(self.counter))
+            self.increment_SP()
+            
         elif command == 'lt':
-            self.f.write('')
+            self.counter += 1
+            self.pop_top_stack()
+            self.f.write('D=M\n')
+            self.pop_top_stack()
+            self.f.write('D=M-D\n')
+            self.f.write('@NOT_LT{}\n'.format(self.counter))
+            self.f.write('D;JGE\n')
+            self.f.write('@SP\n')
+            self.f.write('A=M\n')
+            self.f.write('M=-1\n')
+            self.f.write('@INC_STACK_POINTER_LT{}\n'.format(self.counter))
+            self.f.write('0;JMP\n')
+            self.f.write('(NOT_LT{})\n'.format(self.counter))
+            self.f.write('@SP\n')
+            self.f.write('A=M\n')
+            self.f.write('M=0\n')
+            self.f.write('(INC_STACK_POINTER_LT{})\n'.format(self.counter))
+            self.increment_SP()
         elif command == 'and':
-            self.f.write('')
+            self.f.write('@SP\n')
+            self.f.write('A=M-1\n')
+            self.f.write('D=M\n')
+            self.f.write('A=A-1\n')
+            self.f.write('M=D&M\n')
+            self.f.write('@SP\n')
+            self.f.write('M=M-1\n')
         elif command == 'or':
-            self.f.write('')
+            self.f.write('@SP\n')
+            self.f.write('A=M-1\n')
+            self.f.write('D=M\n')
+            self.f.write('A=A-1\n')
+            self.f.write('M=D|M\n')
+            self.f.write('@SP\n')
+            self.f.write('M=M-1\n')
         elif command == 'not':
-            self.f.write('')
+            self.f.write('@SP\n')
+            self.f.write('A=M-1\n')
+            self.f.write('M=!M\n')
+        else:
+            raise ValueError('Arithmetic command not supported')
 
 
 
