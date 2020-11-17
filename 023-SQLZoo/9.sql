1229cb9148ea5e81d8db8552ab05aff222320f46
akv-akv <drzoob@yandex.ru>
2020-11-17 15:42:20 +0300

    add 023-9

---
+++ 023-SQLZoo/9.sql
@@ -0,0 +1,81 @@
+/* SQLZoo 9 - Self Join - Edinburg buses */
+/* https://www.sqlzoo.net/wiki/Self_join 
+stops(id, name)
+route(num, company, pos, stop)
+*/
+
+
+/* 1 How many stops are in the database.  */
+SELECT COUNT(*)
+FROM stops
+
+/* 2 Find the id value for the stop 'Craiglockhart' */
+SELECT id 
+FROM stops
+WHERE name = 'Craiglockhart'
+
+/* 3 Give the id and the name for the stops on the '4' 'LRT' service. */
+SELECT id,name 
+FROM route
+JOIN stops ON id = stop
+WHERE num = 4
+ORDER BY pos
+
+/* 4 The query shown gives the number of routes that visit either London Road (149) or Craiglockhart (53). Run the query and notice the two services that link these stops have a count of 2. Add a HAVING clause to restrict the output to these two routes. */
+SELECT company, num, COUNT(*)
+FROM route WHERE stop=149 OR stop=53
+GROUP BY company, num
+HAVING COUNT(*) > 1
+
+/* 5 Execute the self join shown and observe that b.stop gives all the places you can get to from Craiglockhart, without changing routes. Change the query so that it shows the services from Craiglockhart to London Road. */
+SELECT a.company, a.num, a.stop, b.stop
+FROM route a 
+JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+JOIN stops sa ON sa.id = a.stop
+JOIN stops sb ON sb.id = b.stop
+WHERE sa.name = 'Craiglockhart' AND sb.name = 'London Road'
+
+/* 6 The query shown is similar to the previous one, however by joining two copies of the stops table we can refer to stops by name rather than by number. Change the query so that the services between 'Craiglockhart' and 'London Road' are shown. If you are tired of these places try 'Fairmilehead' against 'Tollcross' */
+SELECT a.company, a.num, stopa.name, stopb.name
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart' and stopb.name = 'London Road'
+
+/* 7 Give a list of all the services which connect stops 115 and 137 ('Haymarket' and 'Leith') */
+SELECT DISTINCT r1.company,r1.num FROM route r1
+JOIN route r2
+WHERE r1.stop = 115 and r2.stop = 137 AND r1.num = r2.num
+
+/* 8 Give a list of the services which connect the stops 'Craiglockhart' and 'Tollcross' */
+SELECT r1.company, r1.num
+FROM route r1
+JOIN route r2 ON r1.num = r2.num
+JOIN stops sa ON sa.id = r1.stop
+JOIN stops sb ON sb.id = r2.stop
+WHERE sa.name = 'Craiglockhart' AND sb.name = 'Tollcross'
+
+/* 9 Give a distinct list of the stops which may be reached from 'Craiglockhart' by taking one bus, including 'Craiglockhart' itself, offered by the LRT company. Include the company and bus no. of the relevant services. */
+SELECT DISTINCT sb.name, r1.company, r1.num
+FROM route r1
+JOIN route r2 ON r1.num = r2.num AND r1.company = r2.company
+JOIN stops sa ON sa.id = r1.stop
+JOIN stops sb ON sb.id = r2.stop
+WHERE sa.name = 'Craiglockhart'
+
+/* 10 Find the routes involving two buses that can go from Craiglockhart to Lochend.
+Show the bus no. and company for the first bus, the name of the stop for the transfer,
+and the bus no. and company for the second bus. */
+SELECT DISTINCT r1.num, r1.company, conn.name, r3.num, r3.company
+FROM route r1
+JOIN route r2 ON r1.num = r2.num AND r1.company = r2.company
+JOIN stops start ON r1.stop = start.id
+JOIN stops conn ON r2.stop = conn.id
+JOIN route r3 ON r3.stop = conn.id
+JOIN route r4 ON r3.num = r4.num AND r3.company = r4.company
+JOIN stops end ON r4.stop = end.id
+WHERE start.name = 'Craiglockhart' AND end.name = 'Lochend'
+ORDER BY r1.num, conn.id, r2.num, r2.company DESC
+
