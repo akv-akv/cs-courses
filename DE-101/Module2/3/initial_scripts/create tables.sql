b74a1114c896c0b05d6a900c5cf25a70b73209d1
akv-akv <drzoob@yandex.ru>
2021-12-19 00:44:29 +0300

    change schema for initial tables

--- DE-101/Module2/3/initial_scripts/create tables.sql
+++ DE-101/Module2/3/initial_scripts/create tables.sql
@@ -1,6 +1,8 @@
-
-DROP TABLE IF EXISTS orders;
-CREATE TABLE orders(
+CREATE SCHEMA stage;
+CREATE SCHEMA dm;
+SET search_path TO "$user",public,stage,dm;
+DROP TABLE IF EXISTS stage.orders;
+CREATE TABLE stage.orders(
    Row_ID        INTEGER  NOT NULL PRIMARY KEY 
   ,Order_ID      VARCHAR(14) NOT NULL
   ,Order_Date    DATE  NOT NULL
@@ -23,19 +25,19 @@ CREATE TABLE orders(
   ,Discount      NUMERIC(4,2) NOT NULL
   ,Profit        NUMERIC(21,16) NOT NULL
 );
-DROP TABLE IF EXISTS people;
-CREATE TABLE people(
+DROP TABLE IF EXISTS stage.people;
+CREATE TABLE stage.people(
    Person VARCHAR(17) NOT NULL PRIMARY KEY
   ,Region VARCHAR(7) NOT NULL
 );
-DROP TABLE IF EXISTS returns;
-CREATE TABLE returns(
+DROP TABLE IF EXISTS stage.returns;
+CREATE TABLE stage.returns(
    Returned   VARCHAR(3) NOT NULL 
   ,Order_ID   VARCHAR(14) NOT NULL 
 );
-COPY orders
+COPY stage.orders
 FROM '/var/lib/postgresql/csv/orders.csv' DELIMITER ',' CSV HEADER;
-COPY people
+COPY stage.people
 FROM '/var/lib/postgresql/csv/people.csv' DELIMITER ',' CSV HEADER;
-COPY "returns"
+COPY stage.returns
 FROM '/var/lib/postgresql/csv/returns.csv' DELIMITER ',' CSV HEADER;
