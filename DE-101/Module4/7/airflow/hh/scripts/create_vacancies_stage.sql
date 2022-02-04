100e771c0d650dd674eb81a25e19d2a47250b1a8
akv-akv <drzoob@yandex.ru>
2022-02-05 00:17:21 +0300

    add database scripts

---
+++ DE-101/Module4/7/airflow/hh/scripts/create_vacancies_stage.sql
@@ -0,0 +1,20 @@
+CREATE TABLE stg.hh_vacancies
+    (
+        id INT
+        , name VARCHAR(255)
+        , published_at TIMESTAMP
+        , key_skills TEXT
+        , description TEXT
+        , schedule_name VARCHAR(255)
+        , experience_name VARCHAR(255)
+        , area_name VARCHAR(255)
+        , employer_name VARCHAR(255)
+        , url VARCHAR(500)
+        , salary_to INT
+        , salary_from INT
+        , salary_currency VARCHAR(10)
+        , salary_gross BOOLEAN
+        , has_test BOOLEAN
+        , type_name VARCHAR(100)
+        , updated_at TIMESTAMP DEFAULT current_timestamp
+    )
